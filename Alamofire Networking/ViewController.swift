d36d02a8c16964a05f58dfd1f4f8033b383adeae
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-02-21 14:12:56 +0100

    Simple GET and POST requests implemented

--- Alamofire Networking/ViewController.swift
+++ Alamofire Networking/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import Alamofire
+
+let API = "https://jsonplaceholder.typicode.com"
+let GET_POST_ENDPOINT = "/posts/1"
+let POST_ENDPOINT = "/posts"
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        getPost(completion: { post in
+            if let post = post {
+                print("You got post: ")
+                print(post + "\n")
+            }
+        })
+        postRequest()
     }
-
-
+    
+    private func getPost(completion: @escaping (String?) -> Void) {
+        
+        AF.request(API + GET_POST_ENDPOINT, method: .get).validate().response { response in
+            
+            guard let statusCode = response.response?.statusCode else {
+                return
+            }
+            
+            switch statusCode {
+            case 200...299:
+                guard let data = response.data,
+                      let json = try? JSONSerialization.jsonObject(with: data) else {
+                    return
+                }
+                if let postDictionary = json as? [String: Any] {
+                    if let post = postDictionary["body"] as? String {
+                        completion(post)
+                    }
+                }
+            default:
+                print("Error occured.")
+                return
+            }
+        }
+    }
+    
+    private func postRequest() {
+        
+        AF.request(API + POST_ENDPOINT, method: .post).validate().response { response in
+            
+            switch response.result {
+            case .success(_):
+                print("Your POST request was successful.")
+            case .failure(_):
+                print("Your POST request was not successful.")
+            }
+        }
+    }
 }
 
